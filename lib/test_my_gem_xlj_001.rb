878812513b1176290b1b5c62e7cd7cfa53b21091
Xiaolong Jiang <xljiangtju2010@gmail.com>
2017-05-14 12:36:51 -0400

    Version 0.1.0

--- lib/test_my_gem_xlj_001.rb
+++ lib/test_my_gem_xlj_001.rb
@@ -1,5 +1,10 @@
 require "test_my_gem_xlj_001/version"
+require "test_my_gem_xlj_001/hail"
 
 module TestMyGemXlj001
   # Your code goes here...
+  def self.hi(n = "Default Name")
+    hail = Hail
+    Hail.name(n)
+  end
 end
